7ad4615b370f1df1f51a55c2340bcdf00dcf69f6
Josh-PC\Josh <joshmnd8@gmailcom>
2016-09-15 01:10:51 +0100

    Finished the dialogue system

---
+++ Assets/Scripts/NPC/NPCScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+[RequireComponent(typeof(Text))]
+public class NPCScript : MonoBehaviour
+{
+    public Animator bblanim;
+    public Sprite npcImage;
+    public string npcName;
+
+    private Text dialogue;
+
+    private DialogueSystem dialogueSystem;
+
+	void Awake ()
+    {
+        dialogueSystem = GameObject.FindGameObjectWithTag("Dialogue Holder").GetComponent<DialogueSystem>();
+        dialogue = GetComponent<Text>();
+	}
+
+
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            dialogueSystem.npcSprite.sprite = npcImage;
+            dialogueSystem.npcName.text = npcName;
+            dialogueSystem.npcDialogue = dialogue.text;
+            bblanim.SetBool("IsColliding", true);
+            dialogueSystem.canInteract = true;
+
+        }
+    }
+
+
+    void OnTriggerExit2D()
+    {
+       bblanim.SetBool("IsColliding", false);
+        dialogueSystem.canInteract = false;
+    }
+
+}
+
--- Assets/Scripts/UI/DialogueSystem.cs
+++ Assets/Scripts/UI/DialogueSystem.cs
@@ -4,120 +4,65 @@ using UnityEngine.UI;
 public class DialogueSystem : MonoBehaviour
 {
     public GameObject dialogueBox;
-
-    public string[] dialogueText;
-    public float textSpeed;
-    public float speedMultiplier;
-
-    public string npcName;
-    public Sprite npcSprite;
-
-    public GameObject continueDialogue;
-    public GameObject endDialogue;
-
+    public string npcDialogue;
+    [HideInInspector]
+    public bool canInteract;
+    public Text npcName;
+    public Image npcSprite;
 
     private Text dialogue;
-    private int characterCount;
-
-    private bool hasComplete;
-    private bool hasDialogueCompleted;
-
+    private bool isTalking;
+    private PlayerController playerController;
 
     void Awake()
     {
+        npcName = GameObject.FindGameObjectWithTag("Dialogue").transform.GetChild(2).GetComponent<Text>();
+        npcSprite = GameObject.FindGameObjectWithTag("Dialogue").transform.GetChild(1).GetComponent<Image>();
         dialogue = GameObject.FindGameObjectWithTag("Dialogue").transform.GetChild(3).GetComponent<Text>();
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
     }
 
     void Start()
     {
-        HideIcons();
-        GameObject.FindGameObjectWithTag("Dialogue").transform.GetChild(1).GetComponent<Image>().sprite = npcSprite;
-        GameObject.FindGameObjectWithTag("Dialogue").transform.GetChild(2).GetComponent<Text>().text = npcName;
-        textSpeed = 0.5f;
-        speedMultiplier = 0.1f;
-        characterCount = 0;
-        hasComplete = false;
-        hasDialogueCompleted = false;
-        StartCoroutine(ShowDialogue());
+        isTalking = false;
+        canInteract = false;
+        dialogueBox.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.Return) && isTalking)
         {
-            characterCount = 0;
-            dialogueBox.SetActive(true);
-            StartCoroutine(ShowDialogue());
+            StopDialogue();
+        }
+        else if(Input.GetKeyDown(KeyCode.Return) && canInteract)
+        {
+            StartDialogue();
         }
 
-        if (Input.GetKeyDown(KeyCode.Return) && hasComplete)
-        {
-            if (characterCount < dialogueText.Length - 1)
-            {
-                hasDialogueCompleted = false;
-                characterCount++;
-                StartCoroutine(ShowDialogue());
-                
-            }
-            else
-            {
-                hasDialogueCompleted = true;
-                characterCount = dialogueText.Length - 1;
 
-                if (Input.GetKeyDown(KeyCode.Return) && hasDialogueCompleted)
-                {
-                    dialogueBox.SetActive(false);
-                    hasDialogueCompleted = false;
-                }
-            }
-        }
     }
 
-    public IEnumerator ShowDialogue()
+    public void StartDialogue()
     {
-        for (int index = 0; index < (dialogueText[characterCount].Length + 1); index++)
-        {
-            HideIcons();
-            hasComplete = false;
-            dialogue.text = dialogueText[characterCount].Substring(0, index);
-
-            if (characterCount >= dialogueText.Length - 1)
-            {
-                hasDialogueCompleted = true;
-            }
-
-            if (Input.GetKey(KeyCode.Return))
-            {
-                yield return new WaitForSeconds(textSpeed * speedMultiplier);
-            }
-            else
-            {
-                yield return new WaitForSeconds(textSpeed);
-
-            }
-        }
-        ShowIcon();
-
-        hasComplete = true;
-        
-        
+        dialogueBox.SetActive(true);
+        dialogue.text = npcDialogue;
+        isTalking = true;
+        playerController.enabled = false;
     }
 
-    private void HideIcons()
+    public void StopDialogue()
     {
-        continueDialogue.SetActive(false);
-        endDialogue.SetActive(false);
+        dialogueBox.SetActive(false);
+        dialogue.text = "";
+        isTalking = false;
+        playerController.enabled = true;
+
     }
 
-    private void ShowIcon()
-    {
-        if (hasDialogueCompleted)
-        {
-            endDialogue.SetActive(true);
-            return;
-        }
 
-        continueDialogue.SetActive(true);
-    }
+
+
 
 }
