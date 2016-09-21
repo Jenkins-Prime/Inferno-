796701da959f8bbab42791153266595a59c39b8b
Josh-PC\Josh <joshmnd8@gmailcom>
2016-09-22 00:40:07 +0100

    Added boundaries and smooth following to the camera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,22 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
-    public bool isFollowing;
-	[SerializeField] float xOffset;
-	[SerializeField] float yOffset;
+public class CameraController : MonoBehaviour
+{
+    public Vector2 margin;
+    public Vector2 smoothing;
 
-	Transform player;
+    private Transform player;
+    private BoxCollider2D cameraBounds;
+    private bool isFollowing;
+    private Vector3 minBorder;
+    private Vector3 maxborder;
 
-	// Use this for initialization
-	void Start () {
+
+    void Awake()
+    {
+        cameraBounds = GameObject.FindGameObjectWithTag("Bounds").GetComponent<BoxCollider2D>();
+    }
+    void Start ()
+    {
+        minBorder = cameraBounds.bounds.min;
+        maxborder = cameraBounds.bounds.max;
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
         isFollowing = true;
 	}
 	
-	// Update is called once per frame
-	void LateUpdate () {
+	void LateUpdate ()
+    {
+
+        float currentXPosition = transform.position.x;
+        float currentYPosition = transform.position.y;
+
         if (isFollowing)
-            transform.position = new Vector3(player.position.x + xOffset, player.position.y + yOffset, transform.position.z);
-	}
+        {
+            if (Mathf.Abs(currentXPosition - player.position.x) > margin.x)
+            {
+                currentXPosition = Mathf.Lerp(currentXPosition, player.position.x, smoothing.x * Time.deltaTime);
+            }
+
+            if (Mathf.Abs(currentYPosition - player.position.y) > margin.y)
+            {
+                currentYPosition = Mathf.Lerp(currentYPosition, player.position.y, smoothing.y * Time.deltaTime);
+            }
+        }
+
+       float cameraWidth = GetComponent<Camera>().orthographicSize * ((float)Screen.width / Screen.height);
+
+        currentXPosition = Mathf.Clamp(currentXPosition, minBorder.x + cameraWidth, maxborder.x - cameraWidth);
+        currentYPosition = Mathf.Clamp(currentYPosition, minBorder.y + GetComponent<Camera>().orthographicSize, maxborder.y - GetComponent<Camera>().orthographicSize);
+
+        transform.position = new Vector3(currentXPosition, currentYPosition, transform.position.z);
+
+    }
+
 }
