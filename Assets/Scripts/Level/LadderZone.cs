8d1da015ca83f728076988518749c3dcd23f3567
bilakispa <bilakispa@gmail.com>
2016-09-23 05:47:13 +0300

    Added a couple sprites to the Player Spritesheet.

--- Assets/Scripts/Level/LadderZone.cs
+++ Assets/Scripts/Level/LadderZone.cs
@@ -2,14 +2,11 @@
 using System.Collections;
 
 public class LadderZone : MonoBehaviour {
-	public EdgeCollider2D topCol;
-
 	PlayerController pController;
 
 	// Use this for initialization
 	void Start () {
 		pController = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerController> ();
-		topCol = GetComponent<EdgeCollider2D> ();
 	}
 	
 	void OnTriggerEnter2D (Collider2D other) {
@@ -22,9 +19,5 @@ public class LadderZone : MonoBehaviour {
 		if (other.tag == "Player") {
 			pController.ExitLadderZone ();
 		}
-	}
-
-	public void SetPlatformTop(bool solid) {
-		topCol.isTrigger = !solid;
 	}
 }
