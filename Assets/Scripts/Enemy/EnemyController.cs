68afdb1758f3c3be34b32769bc4cf81131a22086
bilakispa <bilakispa@gmail.com>
2016-10-11 12:25:41 +0300

    Changed EnemyController and Ground patrol Enemy Movement.

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyController : RaycastController {
+	public CollisionInfo collisions;
+
+	public override void Start() {
+		base.Start ();
+	}
+	
+	public void Move (Vector3 velocity) {
+		UpdateRaycastOrigins ();
+		collisions.Reset ();
+
+		if (velocity.y != 0)
+			VerticalCollisions (ref velocity);
+		if (velocity.x != 0)
+			HorizontalCollisions (ref velocity);
+			
+		transform.Translate (velocity);
+	}
+
+	void HorizontalCollisions(ref Vector3 velocity) {
+		float directionX = Mathf.Sign (velocity.x);
+		float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+		Vector2 rayOrigin;
+		RaycastHit2D hit;
+		for (int i = 0; i < horizontalRayCount - 1; i++) {
+			rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+			rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+			hit = Physics2D.Raycast (rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+			if (hit) {
+				velocity.x = (hit.distance - skinWidth) * directionX;
+				rayLength = hit.distance;
+
+				collisions.left = (directionX == -1);
+				collisions.right = (directionX == 1);
+			}
+		}
+
+		rayOrigin = (directionX == -1) ? raycastOrigins.topLeft : raycastOrigins.topRight;
+		hit = Physics2D.Raycast (rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+		if (hit) {
+			collisions.wallInFront = true;
+		} else if((collisions.left || collisions.right) && collisions.below) {
+			collisions.canJump = true;
+		}
+	}
+
+	void VerticalCollisions(ref Vector3 velocity) {
+		float directionY = Mathf.Sign (velocity.y);
+		float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+		for (int i = 0; i < verticalRayCount; i++) {
+			Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+			rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+			if (hit) {
+				velocity.y = (hit.distance - skinWidth) * directionY;
+				rayLength = hit.distance;
+
+				collisions.below = (directionY == -1);
+				collisions.above = (directionY == 1);
+			}
+		}
+	}
+
+	public struct CollisionInfo {
+		public bool above, below;
+		public bool left, right;
+		public bool canJump;
+		public bool wallInFront;
+
+		public void Reset() {
+			above = below = false;
+			left = right = false;
+			canJump = false;
+			wallInFront = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (EnemyController))]
+public class EnemyMovement : MonoBehaviour {
+	[SerializeField] Vector3 localWaypointStart;
+	[SerializeField] Vector3 localWaypointEnd;
+	[SerializeField] float moveSpeed = 1f;
+	[SerializeField] float jumpHeight = 0.2f;
+	[SerializeField] float timeToJumpApex = 0.1f;
+
+	Vector3 velocity;
+	Vector3 globalWaypointLeft;
+	Vector3 globalWaypointRight;
+	Vector3 targetWaypoint;
+	float moveDirection;
+	float gravity;
+	float jumpVelocity;
+
+	SpriteRenderer rend;
+	EnemyController controller;
+
+	void Start () {
+		rend = GetComponent<SpriteRenderer> ();
+		controller = GetComponent<EnemyController> ();
+
+		if (localWaypointEnd.x < localWaypointStart.x) {
+			globalWaypointLeft = localWaypointEnd + transform.position;
+			globalWaypointRight = localWaypointStart + transform.position;
+			targetWaypoint = globalWaypointLeft;
+			moveDirection = Mathf.Sign (localWaypointEnd.x - localWaypointStart.x);
+		} else if (localWaypointEnd.x > localWaypointStart.x) {
+			globalWaypointLeft = localWaypointStart + transform.position;
+			globalWaypointRight = localWaypointEnd + transform.position;
+			targetWaypoint = globalWaypointRight;
+			moveDirection = Mathf.Sign (localWaypointEnd.x - localWaypointStart.x);
+		} else {
+			moveDirection = 0;
+		}
+			
+		rend.flipX = (moveDirection == -1);
+
+		gravity = -(2f * jumpHeight) / Mathf.Pow (timeToJumpApex, 2f);
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+	}
+
+	void Update () {
+		if ((moveDirection != 0) && (Mathf.Abs(transform.position.x - targetWaypoint.x) < 0.1f || controller.collisions.wallInFront)) {
+			ChangeEnemyDirection ();
+		}
+
+		if (controller.collisions.above || controller.collisions.below)
+			velocity.y = 0f;
+
+		if (controller.collisions.canJump) {
+			velocity.y = jumpVelocity;
+		}
+
+		velocity.x = moveDirection * moveSpeed;
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
+	}
+
+	public void ChangeEnemyDirection() {
+		if (moveDirection == -1) {
+			targetWaypoint = globalWaypointRight;
+			moveDirection = 1;
+			rend.flipX = false;
+		} else {
+			targetWaypoint = globalWaypointLeft;
+			moveDirection = -1;
+			rend.flipX = true;
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.cyan;
+		if (Application.isPlaying) {
+			Gizmos.DrawLine (globalWaypointLeft, globalWaypointRight);
+		} else {
+			Gizmos.DrawLine (localWaypointStart + transform.position, localWaypointEnd + transform.position);
+		}
+	}
+}
