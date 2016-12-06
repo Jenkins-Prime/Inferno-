9952e79af044fc50153ee7da22d1e66b9638d5aa
Jenkins-Prime <ruffdemongames@gmail.com>
2016-12-07 02:36:10 +0800

    Added functional but rough switch.

---
+++ Assets/Scripts/Puzzles/SwitchScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScript : MonoBehaviour {
+
+	public TargetScript target;
+	private Animator anim;
+	public bool sticks;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D()
+	{
+		anim.SetBool ("active", true);
+		target.SwitchActive();
+
+	}
+
+	void OnTriggerExit2D()
+	{
+		if (sticks)
+			return;
+		anim.SetBool ("active", false);
+		target.SwitchInactive();
+	}
+
+}
