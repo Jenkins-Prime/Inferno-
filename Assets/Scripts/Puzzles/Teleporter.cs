babd3e5253c75f35c7110f058e0ca74ea229afb4
Jenkins-Prime <ruffdemongames@gmail.com>
2016-12-08 02:34:25 +0800

    Added Teleporter
    Make sure "adjust" is higher than 0 to avoid graphical bug.

---
+++ Assets/Scripts/Puzzles/Teleporter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	public bool jump;
+	public GameObject target;
+	public float adjustY;
+	public float adjustX;
+
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (!jump) {
+			if (other.tag == "Player") {
+				jump = true;
+				other.gameObject.transform.position = new Vector3 (target.transform.position.x + adjustX, target.transform.position.y + adjustY, 0);
+			}
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (other.tag == "Player") {
+			jump = false;
+		}
+	}
+
+}
