9b4059f6d3214ee0a1894a91379fcdddf005774a
Jenkins-Prime <ruffdemongames@gmail.com>
2016-12-11 22:38:51 +0800

    Moving Portrait prototype.
    (!) Bugs ahoy (!)

---
+++ Assets/LookAtPlayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour {
+
+	private Player player;
+	public float lookX;
+	public float lookY;
+	public GameObject eyes;
+	public GameObject portrait;
+    public float moveBy = 0.010f;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		lookX = player.transform.position.x;
+		lookY = player.transform.position.y;
+
+		if (lookX > portrait.transform.position.x) 
+		{
+			eyes.transform.position = new Vector3 (transform.position.x + moveBy, transform.position.y, transform.position.z);
+		}
+
+		if (lookY > portrait.transform.position.y) 
+		{
+			eyes.transform.position = new Vector3 (transform.position.x, transform.position.y + moveBy, transform.position.z);
+		}
+	
+		if (lookX < portrait.transform.position.x) 
+		{
+			eyes.transform.position = new Vector3 (transform.position.x - moveBy, transform.position.y, transform.position.z);
+		}
+
+
+		if (lookY < portrait.transform.position.y) 
+		{
+			
+			eyes.transform.position = new Vector3 (transform.position.x, transform.position.y - moveBy, transform.position.z);
+		}
+	}
+}
