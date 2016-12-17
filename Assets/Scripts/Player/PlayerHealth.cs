ac2d40584734b9a60fc05fc1da7e029803349072
Josh-PC\Josh <joshmnd8@gmailcom>
2016-12-17 03:41:37 +0000

    Added the health system

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealthContainers;
+    public int startingHealth;
+    public Image[] healthContainers;
+    public Sprite[] health;
+
+    private int currentHealth;
+    private int maxHealth;
+    private int healthPerContainer;
+
+	void Start ()
+    {
+        healthPerContainer = 2;
+
+        currentHealth = startingHealth * healthPerContainer;
+        maxHealth = maxHealthContainers * healthPerContainer;
+        InitialiseHealth();
+	
+	}
+
+    private void InitialiseHealth()
+    {
+        for (int index = 0; index < maxHealthContainers; index++)
+        {
+            if (startingHealth <= index)
+            {
+                healthContainers[index].enabled = false;
+            }
+            else
+            {
+                healthContainers[index].enabled = true;
+            }
+        }
+
+        UpdateHealth();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Damage(1);
+            UpdateHealth();
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            AddOrbs(2);
+        }
+    }
+
+    private void UpdateHealth()
+    {
+        bool isEmpty = false;
+        int playerHealth = 0;
+
+        foreach(Image img in healthContainers)
+        {
+            if (isEmpty)
+            {
+                img.sprite = health[0];
+            }
+            else
+            {
+                playerHealth++;
+
+                if (currentHealth >= playerHealth * healthPerContainer)
+                {
+                    img.sprite = health[health.Length - 1];
+                }
+                else
+                {
+                    int currentOrbHealth = (int)(healthPerContainer - (healthPerContainer * playerHealth - currentHealth));
+                    int healthPerSprite = healthPerContainer / (health.Length - 1);
+                    int healthIndex = currentOrbHealth / healthPerSprite;
+                    img.sprite = health[healthIndex];
+                    isEmpty = true;
+                }
+
+            }
+        }
+    }
+
+    public void Damage(int amount)
+    {
+        currentHealth -= amount;
+
+        currentHealth = Mathf.Clamp(currentHealth, 0, startingHealth * healthPerContainer);
+        UpdateHealth();
+    }
+
+    public void AddOrbs(int amount)
+    {
+        startingHealth += amount;
+        startingHealth = Mathf.Clamp(startingHealth, 0, maxHealthContainers);
+        currentHealth = startingHealth * healthPerContainer;
+        InitialiseHealth();
+    }
+}
