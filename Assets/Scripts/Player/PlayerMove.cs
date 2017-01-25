0b68cef677f90ffdf40b465a5be37caffc99f4cb
Josh-PC\Josh <joshmnd8@gmailcom>
2017-01-25 13:52:58 +0000

    Cleaned up the scene and added sorting layers for the background, midground and foreground

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerMove : MonoBehaviour
+{
+    protected Vector3 velocity;
+    protected Vector2 moveDirection;
+
+    protected SpriteRenderer rend;
+    protected Animator anim;
+    protected ActorController controller;
+    protected EnemyPatrol patrol;
+    protected EnemyChase chase;
+
+    protected virtual void Start ()
+    {
+		
+	}
+	
+    protected abstract void Update();
+    protected abstract void Move();
+    protected abstract void AnimatePlayer();
+}
