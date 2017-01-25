faba3fda641574a47a41746e01cdcf5c5d2009bd
Josh-PC\Josh <joshmnd8@gmailcom>
2017-01-25 02:14:51 +0000

    Updated the parallax effect

---
+++ Assets/Scripts/Camera/ParallaxScrolling.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] backgrounds;
+    [SerializeField]
+    private float parallaxScale;
+    [SerializeField]
+    private float parallaxReductionAmount;
+    [SerializeField]
+    private float parallaxSmoothing;
+
+    private Vector3 lastPosition;
+
+
+
+    void Start ()
+    {
+        lastPosition = transform.position;
+	}
+	
+	void LateUpdate ()
+    {
+        Parallax();
+	}
+
+    private void Parallax()
+    {
+        float parallaxAmount = (lastPosition.x - transform.position.x) * parallaxScale;
+
+        for (int index = 0; index < backgrounds.Length; index++)
+        {
+            float parallaxPosition = backgrounds[index].transform.position.x + parallaxAmount * (index * parallaxReductionAmount + 1);
+            backgrounds[index].position = Vector3.Lerp(backgrounds[index].position, new Vector3(parallaxPosition, backgrounds[index].position.y, backgrounds[index].position.z), parallaxSmoothing * Time.deltaTime);
+        }
+
+        lastPosition = transform.position;
+    }
+}
