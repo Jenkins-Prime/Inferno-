33d15bcd03183dc732fa4885db56d7b93fec1d95
along88 <a.long88@yahoo.com>
2017-03-22 23:39:35 -0400

    ShopKeeper Scene setup
    Created a Shop scene in the project folder
    created ShopKeeper.cs base class in new "Forge" Folder
    Notes added in scripts Comments

---
+++ Assets/Scripts/Forge/ShopKeeper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShopKeeper : MonoBehaviour
+{
+
+//    Shop:
+//player enters shop
+//    can have different types of merchants
+//    one specializes in health
+//    one sales armor.
+
+//a UI shop manager to showcase all the items
+//need a list of items the shop owner has
+//Need a shopkeeper object
+//shopkeep can sell only(for now), has an inventory to sale, items in inventory have a price(value)
+
+//    Note: Perhaps base class item.cs can hold a field for "Value" and possibly "ItemName"
+
+    [SerializeField]
+    private List<string> stock; //should hold a generic list of items
+    [SerializeField]
+    private List<Text> itemDisplay;
+    private Canvas shopMenu;
+    
+    private void Start()
+    {
+        shopMenu = this.GetComponent<Canvas>();
+        itemDisplay = new List<Text>();
+        stock = new List<string>();
+        stock.Add("Health Orb");
+        
+        
+    }
+
+    private void OnTriggerEnter2D(BoxCollider2D col)
+    {
+        
+        
+
+
+    }
+}
